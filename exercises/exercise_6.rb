e1d7f14027cf1e99bd1c537604aa3bda0642e8c0
Alan Marx <alanmarx@hotmail.com>
2020-04-28 16:43:28 +0000

    implemented exercises 6 to 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,34 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create!(
+  first_name: "Khurram", 
+  last_name: "Virani", 
+  hourly_rate: 60)
+
+@store1.employees.create!(
+  first_name: "Patricia", 
+  last_name: "Lennon", 
+  hourly_rate: 100)
+
+@store1.employees.create!(
+  first_name: "John", 
+  last_name: "Ono", 
+  hourly_rate: 200)
+
+@store2.employees.create!(
+  first_name: "Bobby", 
+  last_name: "Jagger", 
+  hourly_rate: 40)
+
+@store2.employees.create!(
+  first_name: "Barry", 
+  last_name: "Garret", 
+  hourly_rate: 65)
+
+@store2.employees.create!(
+  first_name: "Fiona", 
+  last_name: "Lewis", 
+  hourly_rate: 175)
+
+  puts Employee.count
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,7 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter a store name:"
+answer = gets.chomp
+new_store = Store.create(name: answer)
+pp new_store.errors.full_messages
