35e7e9f28ce49d5cfd616e569ca5d0769249ff51
Alan Marx <alanmarx@hotmail.com>
2020-04-28 03:37:05 +0000

    implemented exercises 4 and 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,29 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey_store = Store.create(
+  name: "Surrey", 
+  annual_revenue: 224000, 
+  mens_apparel: false, 
+  womens_apparel: true)
+
+whistler_store = Store.create(
+  name: "Whistler", 
+  annual_revenue: 1900000, 
+  mens_apparel: true, 
+  womens_apparel: false)
+
+yaletown_store = Store.create(
+  name: "Yaletown", 
+  annual_revenue: 430000, 
+  mens_apparel: true, 
+  womens_apparel: true)
+
+@mens_only = Store.where(mens_apparel: true)
+
+@mens_only.each do |i|
+  puts "#{i.name} has an annual revenue of #{i.annual_revenue}"
+end
+
+@womens_under_million = Store.where(["womens_apparel = ? and annual_revenue < ?", "true", "1000000"])
+pp @womens_under_million
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Total store annual revenues amount to $#{Store.sum("annual_revenue")}"
+puts "Average store annual revenues amount to $#{Store.average("annual_revenue")}"
+puts "There are #{Store.where(['annual_revenue >= ?','1000000']).count} stores earning more than $1 million a year in revenues"
